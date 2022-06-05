41b310016a2fde43f9484668e8178b613d6a9828
nanawarechaitali <nanawarechaitali4196@gamil.com>
2022-06-06 04:32:03 +0530

    first commit

---
+++ src/com/bridgelabz/StartingUC1.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class StartingUC1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Program on Master Branch");
+        System.out.println("Initial position of the player1 is at 0");
+
+    }
+}
