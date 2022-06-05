d9d91a5aedeecb855ffc134a50ff8f46361666e9
nanawarechaitali <nanawarechaitali4196@gamil.com>
2022-06-06 04:34:39 +0530

    first commit

--- src/com/bridgelabz/StartingUC1.java
+++ src/com/bridgelabz/StartingUC1.java
@@ -1,9 +1,14 @@
 package com.bridgelabz;
-
+import java.util.Random;
 public class StartingUC1 {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Program on Master Branch");
         System.out.println("Initial position of the player1 is at 0");
+
+
+        Random random = new Random();
+        int dice= random.nextInt(6)+1;
+        System.out.println("Player is rolling dice and get "+dice);
 
     }
 }
